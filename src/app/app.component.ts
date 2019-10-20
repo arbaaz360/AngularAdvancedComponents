a1e2fadc39b3f8912b6def0268e623acd11af03d
arbaaz360 <arbaaz9044@gmail.com>
2019-10-20 20:28:21 +0530

    ViewChild()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,34 @@
-import { Component } from "@angular/core";
+import { SimpleAlertViewComponent } from "./simple-alert-view/simple-alert-view.component";
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  AfterContentInit
+} from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   public isAddTimerVisible: boolean = false;
   public time: number = 0;
   public timers: Array<number> = [];
   public isEndTimerAlertVisible = false;
 
+  @ViewChild(SimpleAlertViewComponent) alert: SimpleAlertViewComponent;
+
   constructor() {
     this.timers = [2, 3, 185];
     console.log(this.timers);
   }
-
+  ngAfterContentInit() {
+    console.log(this.alert);
+    this.alert.show();
+    this.alert.title = "hi";
+    this.alert.message = "hi";
+  }
   logCountDownEnd() {
     console.log("the countdown has finished");
     this.showEndTimerAlert();
